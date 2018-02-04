36127144b5d2333409e9567ce51d449f77fde290
Anton <yorzh.anton@gmail.com>
2018-02-04 04:58:26 -0600

    1. Added EMT standard and improved (yellow) 2. Added plots for EMT st and i1 (red) 3. Improved presentation file (gray)

--- python/TMM_EMT_TI.py
+++ python/TMM_EMT_TI.py
@@ -21,9 +21,10 @@ Material_name = "Bi2Se3-ZnSe"
 N_layers = 18
 N_periods = 4
 
-# Permeability and thickness of layers (nm)
+# Permeability
 mu = np.ones((1, N_layers), dtype=float)
 
+# Thickness of layers for TMM
 d_TMM = np.zeros((1, 4*N_periods+2), dtype = float)
 d_air = 1E12
 d_cond = 0.92*1E-9
@@ -35,28 +36,51 @@ aa_ = np.tile(aa_, 4)
 for i in range(len(d_TMM[0])-2):
     d_TMM[0][i+1] = aa_[i]
 
+# Thickness of layers for EMT
+d_EMT = np.array([ [d_air, (d_cond+d_bulk+d_dielectric)*N_periods, d_air]  ])
+ff = (d_cond+d_bulk)/(d_cond+d_bulk+d_dielectric)
+ffm = d_bulk/(d_cond+d_bulk+d_dielectric)
+ffd = d_dielectric/(d_cond+d_bulk+d_dielectric)
+
 # Setup wavelengths
-#wl = np.zeros((1,500), dtype=float)
-#wl[0] = np.linspace(500,1000, 500)
-#wl = np.array([[550]])
+wl = np.zeros((1,500), dtype=float)
+wl[0] = np.linspace(500,1000, 500)
+#wl = np.array([[400]])
 
 # Select angles of incidence
-theta_i= np.zeros((1,180), dtype=float)
-theta_i[0] = np.linspace(0,89.9,180) # or:
-#theta_i= np.array([[0]])
+#theta_i= np.zeros((1,180), dtype=float)
+#theta_i[0] = np.linspace(0,89.9,180) # or:
+theta_i= np.array([[0]])
 
 # Setup dielectric fn for each layer at different wavelengths
 eps_air = np.array([[1.0]])
+
 eps_dO = np.zeros((1,len(wl[0])), dtype=complex)
 eps_dE = np.zeros((1,len(wl[0])), dtype=complex)
+
 eps_condO = np.zeros((1,len(wl[0])), dtype=complex)
 eps_condE = np.zeros((1,len(wl[0])), dtype=complex)
+
 eps_bulkO = np.zeros((1,len(wl[0])), dtype=complex)
 eps_bulkE = np.zeros((1,len(wl[0])), dtype=complex)
+
+eps_EMTO_st = np.zeros((1,len(wl[0])), dtype=complex)
+eps_EMTE_st = np.zeros((1,len(wl[0])), dtype=complex)
+
+eps_EMTO_i1 = np.zeros((1,len(wl[0])), dtype=complex)
+eps_EMTE_i1 = np.zeros((1,len(wl[0])), dtype=complex)
 
 Rp = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
 Tr = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
 Ab = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
+
+RpEMT_st = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
+TrEMT_st = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
+AbEMT_st = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
+
+RpEMT_i1 = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
+TrEMT_i1 = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
+AbEMT_i1 = np.zeros((len(theta_i[0]),len(wl[0])),dtype=float)
 
 #-------------------------------
 
@@ -76,8 +100,15 @@ for i in range(len(theta_i[0])):
         eps_bulkO[0][j] = get_eps_Bi2Se3_bulk_Wolf(wl[0][j])
         eps_bulkE[0][j] = get_eps_Bi2Se3_bulk_Wolf(wl[0][j])
 
-        #eps_bulkO[0][j] = complex(get_eps_Bi2Se3_bulk_Yin(wl[0][j])[0],get_eps_Bi2Se3_bulk_Yin(wl[0][j])[1])
-        #eps_bulkE[0][j] = complex(get_eps_Bi2Se3_bulk_Yin(wl[0][j])[0],get_eps_Bi2Se3_bulk_Yin(wl[0][j])[1])
+        eps_EMTO_st[0][j] = ff*eps_bulkO[0][j]+(1.0-ff)*eps_dO[0][j]
+        eps_EMTE_st[0][j] = eps_bulkE[0][j]*eps_dE[0][j]/(ff*eps_dE[0][j]+(1-ff)*eps_bulkE[0][j])
+        eps_EMTO_i1[0][j] = ffm*eps_bulkO[0][j]+ ffd*eps_dO[0][j] + (1-ffm-ffd)*eps_condO[0][j]
+        eps_EMTE_i1[0][j] = 1.0/(ffm/eps_bulkO[0][j] + ffd/eps_dO[0][j] + (1-ffm-ffd)/eps_condE[0][j])
+
+        eps_EMT_O_st = np.array([ [eps_air, eps_EMTO_st[0][j], eps_air] ])
+        eps_EMT_E_st = np.array([ [eps_air, eps_EMTE_st[0][j], eps_air] ])
+        eps_EMT_O_i1 = np.array([ [eps_air, eps_EMTO_i1[0][j], eps_air] ])
+        eps_EMT_E_i1 = np.array([ [eps_air, eps_EMTE_i1[0][j], eps_air] ])
 
         #Epsilon of period (dielectric, conduction, bulk, conduction)
         eps_period_O = np.array([
@@ -108,26 +139,43 @@ for i in range(len(theta_i[0])):
 
         Ap, Bp = get_A_B(d_TMM, N_layers, wl[0][j], eps_TMM_O, eps_TMM_E, kx, mu)
 
+        ApEMT_st, BpEMT_st = get_A_B(d_EMT, 3, wl[0][j], eps_EMT_O_st, eps_EMT_E_st, kx, mu)
+        ApEMT_i1, BpEMT_i1 = get_A_B(d_EMT, 3, wl[0][j], eps_EMT_O_i1, eps_EMT_E_i1, kx, mu)
+
         Rp_TM = pow(np.abs(Bp[0][0]/Ap[0][0]),2)
         Tr_TM = np.real(kz_end/eps_TMM_O[-1][0])/np.real(kz_air/eps_TMM_O[0][0])*pow(np.abs(Ap[0][-1]/Ap[0][0]), 2)
         Ab_TM = 1 - Rp_TM - Tr_TM
+
+        Rp_EMT_st = pow(np.abs(BpEMT_st[0][0]/ApEMT_st[0][0]),2)
+        Tr_EMT_st = pow(np.abs(ApEMT_st[0][-1]/ApEMT_st[0][0]),2)
+        #Tr_EMT_st = np.real(kz_end/eps_EMT_O_st[-1][0])/np.real(kz_air/eps_EMT_O_st[0][0])*pow(np.abs(ApEMT_st[0][-1]/ApEMT_st[0][0]), 2)
+        Ab_EMT_st = 1 - Rp_EMT_st - Tr_EMT_st
+
+        Rp_EMT_i1 = pow(np.abs(BpEMT_i1[0][0]/ApEMT_i1[0][0]),2)
+        Tr_EMT_i1 = pow(np.abs(ApEMT_i1[0][-1]/ApEMT_i1[0][0]),2)
+        Ab_EMT_i1 = 1 - Rp_EMT_i1 - Tr_EMT_i1
 
         Rp[i][j] = Rp_TM
         Tr[i][j] = Tr_TM
         Ab[i][j] = Ab_TM
 
+        RpEMT_st[i][j] = Rp_EMT_st
+        TrEMT_st[i][j] = Tr_EMT_st
+        AbEMT_st[i][j] = Ab_EMT_st
+
+        RpEMT_i1[i][j] = Rp_EMT_i1
+        TrEMT_i1[i][j] = Tr_EMT_i1
+        AbEMT_i1[i][j] = Ab_EMT_i1
+
+
 print
 print "Material:", Material_name
 print "Overal number of layers:", N_layers
 print "Period number:", N_periods
-print "Structure:", d_TMM
-#print "Tested incidence angles:", theta_i[0]
-#print "Tested wavelengths,[nm]:", wl[0], '\n'
+#print "TMM Structure:", d_TMM
 
 #print "Rp:", Rp
-#print
 #print "Tp:", Tr
-#print
 #print "Ab:", Ab
 
 #print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
@@ -142,45 +190,48 @@ print "Structure:", d_TMM
 #-------------------------------
 # 1. Perform plotting magic
 def plot_Rp_Tp(ax, ay, xaxis, R, T):
-    ax.plot(xaxis[:], T[:], label= 'Transmittance', color = 'r')
-    ax.plot(xaxis[:], T[:], color='r')
+    ax.plot(xaxis[:], Ti[:], label= 'Transmittance', color = 'r')
+    #ax.plot(xaxis[:], Tst[:], label= 'T "Standard"', color = 'r', linestyle=":")
+    #ax.plot(xaxis[:], Ti[:], color='r')
     ax.yaxis.tick_left()
     ax.set_ylabel('Transmittance, $T$', color = 'r')
     ax.tick_params('y',colors='r')
 
     ay = ax.twinx()
-    ay.plot(xaxis[:], R[:], label= 'Reflectance', color = 'b')
-    ay.plot(xaxis[:], R[:], color='b')
+    ay.plot(xaxis[:], Ri[:], label= 'Reflectance', color = 'b')
+    #ay.plot(xaxis[:], Rst[:], label= 'R "Standard"', color = 'b', linestyle=":")
+    #ay.plot(xaxis[:], R[:], color='b')
     ay.set_ylabel('Reflectance, $R$', color = 'b')
     ay.tick_params('y',colors='b')
 
-    ax.set_xlabel('Incidence angle theta, 'r'$\theta$')
-    #ax.set_xlabel('Wavelength, 'r'$\lambda$')
+    #ax.set_xlabel('Incidence angle theta, 'r'$\theta$')
+    ax.set_xlabel('Wavelength, 'r'$\lambda$')
+
     ax.legend(loc=2, fancybox=True)
+    ay.legend(loc=1, fancybox=True)
     #ax.legend(bbox_to_anchor=(0, 0.99, 1, 0), loc=2)
     #ay.legend(bbox_to_anchor=(0, 0.9, 1, 0),  loc=2)
-    ay.legend(loc=1, fancybox=True)
 
     ymajor_ticks = np.arange(0, 1.01, 0.2)
     yminor_ticks = np.arange(0, 1.02, 0.02)
 
     # 1 angle - many wl:
-    #xmajor_ticks = np.arange(500, 1100, 100)
-    #xminor_ticks = np.arange(500, 1010, 10)
+    xmajor_ticks = np.arange(500, 1100, 100)
+    xminor_ticks = np.arange(500, 1010, 10)
 
     # 1 wl - many angles:
-    xmajor_ticks = np.arange(0, 100, 10)
-    xminor_ticks = np.arange(0, 91, 1)
+    #xmajor_ticks = np.arange(0, 100, 10)
+    #xminor_ticks = np.arange(0, 91, 1)
     ax.set_yticks(ymajor_ticks)
     ax.set_yticks(yminor_ticks, minor = True)
     ay.set_yticks(ymajor_ticks)
     ay.set_yticks(yminor_ticks, minor = True)
     ax.set_xticks(xmajor_ticks)
     ax.set_xticks(xminor_ticks, minor = True)
-    pl.text(0.5, 0.85,"$\lambda$ = 550 nm", horizontalalignment = 'center', verticalalignment = 'center',
-            transform = ax.transAxes)
-    #pl.text(0.5, 0.85,"$\ theta$ = 0", horizontalalignment = 'center', verticalalignment = 'center',
+    #pl.text(0.5, 0.85,"$\lambda$ = 400 nm", horizontalalignment = 'center', verticalalignment = 'center',
     #        transform = ax.transAxes)
+    pl.text(0.5, 0.85,"$\ theta$ = 0", horizontalalignment = 'center', verticalalignment = 'center',
+            transform = ax.transAxes)
 
 def doFigure_RTA(xaxis, R, x):
     fig = pl.figure()
@@ -188,50 +239,73 @@ def doFigure_RTA(xaxis, R, x):
     ayR = fig.add_subplot(111)
     plot_Rp_Tp(axR, ayR, xaxis, R, x)
     fig.tight_layout()
-    fig.savefig('plots/plot_R_T_per_theta_i.pdf')
-    fig.savefig('plots/plot_R_T_per_theta_i.png', dpi=500)
+    #fig.savefig('../plots/plot_R_T_EMT_i1_vs_st(0 deg).png', dpi=500)
     return
 
 R =[]
 T =[]
 A =[]
 
-#fixed angle - many wl:
+#EMT: fixed angle - many wl:
+for i in range(len(RpEMT_i1[0])):
+    R.append(RpEMT_i1[0][i])
+    T.append(TrEMT_i1[0][i])
+    A.append(AbEMT_i1[0][i])
+
+#EMT: fixed wl - many angles:
+#for i in range(len(RpEMT_i1)):
+#    R.append(RpEMT_i1[i][0])
+#    T.append(TrEMT_i1[i][0])
+#    A.append(AbEMT_i1[i][0])
+#------
+#TMM: fixed angle - many wl:
 #for i in range(len(Rp[0])):
 #    R.append(Rp[0][i])
 #    T.append(Tr[0][i])
 #    A.append(Ab[0][i])
 
-#fixed wl - many angles:
-for i in range(len(Rp)):
-    R.append(Rp[i][0])
-    T.append(Tr[i][0])
-    A.append(Ab[i][0])
+#TMM: fixed wl - many angles:
+#for i in range(len(Rp)):
+#    R.append(Rp[i][0])
+#    T.append(Tr[i][0])
+#    A.append(Ab[i][0])
 
 # Select theta_i[0] or wl[0]:
-#doFigure_RTA(theta_i[0], R, T)
+#doFigure_RTA(wl[0], R, T)
 #pl.show()
 
-def plot_Eps(ax, wl, epsE, epsO):
-    ax.plot(wl, epsE, label= "Epsilon extraordinary", color='r')
-    ax.plot(wl, epsO, label= "Epsilon ordinary", color='c')
+# Attention - works only for 1 theta and range of wavelengths
+def plot_Eps(ax, wl, epsER, epsOR, epsEI, epsOI):
+    ax.plot(wl, epsER, label= "Extraordinary, real", color='r')
+    ax.plot(wl, epsOR, label= "Ordinary, real", color='b')
+
+    ax.plot(wl, epsEI, color='r', linestyle=':',  label='Extraordinary, imaginary')
+    ax.plot(wl, epsOI, color='b', linestyle=':', label='Ordinary, imaginary')
     ax.plot(wl, wl*0, linestyle = '-.', color='grey', linewidth = 0.3)
     ax.set_ylabel('Epsilon, 'r'$\epsilon$')
-    ax.minorticks_on()
-    ax.set_xlabel('Wavelength, 'r'$\lambda$')
+    ax.set_xlabel('Wavelength, 'r'$\lambda$ [nm]')
+    #ax.minorticks_on()
+    xmajor_ticks = np.arange(500, 1100, 100)
+    xminor_ticks = np.arange(500, 1010, 10)
+    ymajor_ticks = np.arange(-1, 5.5, 1)
+    yminor_ticks = np.arange(-1, 5.1, 0.1)
 
+    ax.set_xticks(xmajor_ticks)
+    ax.set_xticks(xminor_ticks, minor = True)
+    ax.set_yticks(ymajor_ticks)
+    ax.set_yticks(yminor_ticks, minor = True)
     ax.legend(loc='best',fancybox=True)
-    pl.text(0.1, 0.75,"$\ theta$ = 0", horizontalalignment = 'center', verticalalignment = 'center',
+    pl.text(0.8, 0.9,"$\ theta$ = 0", horizontalalignment='center', verticalalignment = 'center',
         transform = ax.transAxes)
 
-def doFigure_Eps(wl, epsE, epsO):
+def doFigure_Eps(wl, epsER, epsOR, epsEI, epsOI):
     fig = pl.figure()
     axEps = fig.add_subplot(111)
-    plot_Eps(axEps, wl,epsE,epsO)
-    fig.suptitle('Dielectric function of bulk', fontsize=10)
+    plot_Eps(axEps, wl, epsER, epsOR, epsEI, epsOI)
+    fig.suptitle('Dielectric function calculated using improved EMT', fontsize=10)
     #fig.tight_layout()
-    fig.savefig('plot_Eps_bulk.pdf')
+    fig.savefig('../plots/plot_Eps_EMT_improved1.png', dpi=500)
     return
 
-#doFigure_Eps(wl[0], eps_condE[0], eps_condO[0])
+#doFigure_Eps(wl[0], eps_EMTE_i1[0].real, eps_EMTO_i1[0].real, eps_EMTE_i1[0].imag, eps_EMTO_i1[0].imag)
 #pl.show()
