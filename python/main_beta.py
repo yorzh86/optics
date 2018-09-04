b22c20a93739a2239673f6be504d1729959ba0fe
Anton <yorzh.anton@gmail.com>
2018-09-03 21:55:20 -0500

    added files

---
+++ python/main_beta.py
@@ -0,0 +1,24 @@
+from __future__ import division
+import numpy as np
+import sys
+from math import *
+import cmath
+from TMM_EMT_TI import calculateRpTrAb
+#import postprocess
+
+
+#1. Read configurations ('Bi2Se3.configs' or 'Bi2Te3.configs')
+cfg = np.array(pl.loadtxt('Bi2Se3.configs',skiprows=3))
+#=============================
+#N| d_Bulk| d_Substr| d_Total|
+#=============================
+#1     100     100     2000
+#2     30     50     2000
+#3     100     10     2000
+#4     12     10     2000
+
+print cfg
+#2. Setup which configurations to run and what resolution to use
+for i in range(len(cfg[0])):
+    calculateRpTrAb(cfg[i][1], cfg[i][2], cfg[i][3], 200, 180)
+    # (substrate, ti, total, wl_reso, angle_reso - optional)
